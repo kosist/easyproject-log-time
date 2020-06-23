060fb1f31a81ee313eafd43034e756ce89695141
Ivan <kosistivan@gmail.com>
2020-06-23 22:45:20 +0200

    Added validation based on number of digits after decimal separation

--- UI/Rule/TimeFormatRule.cs
+++ UI/Rule/TimeFormatRule.cs
@@ -11,16 +11,19 @@ namespace UI.Rule
             double result = 0.0;
             string text = (string) value;
 
-            var pattern = @"^(?:0?[0-9]|1[0-9]|2[0-9]):[0-9][0-9]$";
-            Regex rg = new Regex(pattern);
+            var timeFormatPattern = @"^(?:0?[0-9]|1[0-9]|2[0-9]):[0-9][0-9]$";
+            Regex timeFormatRegex = new Regex(timeFormatPattern);
+            bool timeFormatRegexValid = timeFormatRegex.IsMatch(text);
 
-            bool regexValid = rg.IsMatch(text);
+            var doubleFormatPattern = $@"^(\d+\{cultureInfo.NumberFormat.NumberDecimalSeparator}?\d{{0,2}}$)[\d.]{{0,2}}$";
+            Regex doubleFormatRegex = new Regex(doubleFormatPattern);
+            bool doubleFormatRegexValid = doubleFormatRegex.IsMatch(text);
 
             bool validFlag = false;
             string groupSeparator = cultureInfo.NumberFormat.NumberGroupSeparator;
             bool canConvert = double.TryParse(text, out result);
             bool foundGroupSeparator = text.Contains(groupSeparator);
-            validFlag =!foundGroupSeparator && (canConvert || regexValid);
+            validFlag =!foundGroupSeparator && ((canConvert && doubleFormatRegexValid) || timeFormatRegexValid);
             return new ValidationResult(validFlag, "Not a valid time format");
         }
     }
