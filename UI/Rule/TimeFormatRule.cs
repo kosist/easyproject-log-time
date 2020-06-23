4527525785b0c55e2f7c7c7e0138062387ee7603
Ivan <kosistivan@gmail.com>
2020-06-23 22:27:47 +0200

    Updated validation using regex

--- UI/Rule/TimeFormatRule.cs
+++ UI/Rule/TimeFormatRule.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Text.RegularExpressions;
 using System.Windows.Controls;
 
 namespace UI.Rule
@@ -9,11 +10,17 @@ namespace UI.Rule
         {
             double result = 0.0;
             string text = (string) value;
+
+            var pattern = @"^(?:0?[0-9]|1[0-9]|2[0-9]):[0-9][0-9]$";
+            Regex rg = new Regex(pattern);
+
+            bool regexValid = rg.IsMatch(text);
+
             bool validFlag = false;
             string groupSeparator = cultureInfo.NumberFormat.NumberGroupSeparator;
             bool canConvert = double.TryParse(text, out result);
             bool foundGroupSeparator = text.Contains(groupSeparator);
-            validFlag = canConvert && !foundGroupSeparator;
+            validFlag =!foundGroupSeparator && (canConvert || regexValid);
             return new ValidationResult(validFlag, "Not a valid time format");
         }
     }
