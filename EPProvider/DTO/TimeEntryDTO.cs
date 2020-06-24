e7df1b49628f870ad86c13c95545e21c0738eda1
Ivan Yakushchenko <ivan.yakushchenko@anv-tech.com>
2020-06-24 09:33:43 +0200

    Added DTO for time entry class

---
+++ EPProvider/DTO/TimeEntryDTO.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp.Serializers;
+
+namespace EPProvider.DTO
+{
+    [SerializeAs(Name = "time_entry")]
+    public class TimeEntryDTO
+    {
+        [SerializeAs(Name = "project_id")]
+        public int ProjectId { get; set; }
+
+        [SerializeAs(Name = "issue_id")]
+        public int IssueId { get; set; }
+
+        [SerializeAs(Name ="hours")]
+        public double SpentTime { get; set; }
+
+        [SerializeAs(Name ="comments")]
+        public string Description { get; set; }
+
+        [SerializeAs(Name = "spent_on")]
+        public string SpentOnDate { get; set; }
+    }
+}
