da852f0f803d1650c5401de850c5278366739702
Ivan <kosistivan@gmail.com>
2020-10-05 20:39:22 +0200

    Implemented IEquatable interface in order to apply Distinct method on the Users

--- BaseLayer/Model/User.cs
+++ BaseLayer/Model/User.cs
@@ -1,8 +1,35 @@
-﻿namespace BaseLayer.Model
+﻿using System;
+
+namespace BaseLayer.Model
 {
-    public class User
+    public class User : IEquatable<User>
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public bool Equals(User other)
+        {
+            //Check whether the compared object is null.
+            if (Object.ReferenceEquals(other, null)) return false;
+
+            //Check whether the compared object references the same data.
+            if (Object.ReferenceEquals(this, other)) return true;
+
+            //Check whether the users' properties are equal.
+            return Id.Equals(other.Id) && Name.Equals(other.Name);
+        }
+
+        public override int GetHashCode()
+        {
+
+            //Get hash code for the Name field if it is not null.
+            int hashUserName = Name == null ? 0 : Name.GetHashCode();
+
+            //Get hash code for the Id field.
+            int hashUserId = Id.GetHashCode();
+
+            //Calculate the hash code for the user.
+            return hashUserName ^ hashUserId;
+        }
     }
 }
