754b5f8a767d11b9450810c8cbc474fe740043ca
Ivan <kosistivan@gmail.com>
2020-10-12 23:22:35 +0200

    Added empty (as per now) mock class

---
+++ MockProvider/MockEpProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BaseLayer.Model;
+using EPProvider;
+
+namespace MockProvider
+{
+    public class MockEpProvider : IEPProvider
+    {
+        public async Task<(OperationStatusInfo status, List<Project> projectsList)> GetProjectsListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Issue>> GetIssuesListForProjectAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<OperationStatusInfo> AddTimeEntry(TimeEntry timeEntryData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<OperationStatusInfo> CredentialsValid()
+        {
+            return new OperationStatusInfo
+            {
+                OperationMessage = "Login is valid",
+                OperationStatus = true
+            };
+        }
+
+        public async Task<List<TimeEntry>> GetTimeEntries(DateTime date, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<(OperationStatusInfo status, int userId)> GetCurrentUserId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<User>> GetProjectUsersListAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<OperationStatusInfo> UpdateIssueStatus(UpdatedIssue issue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<User>> GetUsersList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Playground/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputText = Console.ReadLine();
+            var inputNumber = 0;
+            // try to parse - if success, then parsingFlag = True
+            // and parsed number is stored to inputNumber
+            var parsingFlag = Int32.TryParse(inputText, out inputNumber);
+            if ((inputNumber >= 1000) && (inputNumber <= 9999))
+            {
+                // do your algorithm
+            }
+            else
+                Console.WriteLine("Invalid entry");
+
+        }
+    }
+}
