4c5db43b770a949b579c7d118eac228982d617c1
Ivan <kosistivan@gmail.com>
2020-10-13 23:22:27 +0200

    Added missing profile mapping

---
+++ MockProvider/Mapping/UpdatedIssueMockMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BaseLayer.Model;
+
+namespace MockProvider
+{
+    public class UpdatedIssueMockMapperProfile : Profile
+    {
+        public UpdatedIssueMockMapperProfile()
+        {
+            CreateMap<UpdatedIssue, UpdatedIssueMockDto>();
+            CreateMap<UpdatedIssueMockDto, UpdatedIssue>();
+        }
+    }
+}
