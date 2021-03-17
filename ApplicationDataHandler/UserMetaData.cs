121c64a89ed5c3f5564b601369ae6474d2b2eb49
Ivan Yakushchenko <ivan.yakushchenko@anv-tech.com>
2021-03-17 15:16:14 +0100

    No need to have Name, Surname and FullName - Name is enough

--- ApplicationDataHandler/UserMetaData.cs
+++ ApplicationDataHandler/UserMetaData.cs
@@ -8,14 +8,7 @@ namespace ApplicationDataHandler
     {
         public int UserId { get; set; }
         public string Name { get; set; }
-        public string Surname { get; set; }
-        public string FullName { get; private set; }
         public string UserLogin { get; set; }
         public bool LoggedInUser { get; set; }
-
-        public UserMetaData()
-        {
-            FullName = Name + " " + Surname;
-        }
     }
 }
