931630b7bf3e51690f34e8da7d1c29573bb08637
Ivan Yakushchenko <ivan.yakushchenko@anv-tech.com>
2021-03-17 15:14:45 +0100

    Added class which has to be implemented to handle JSON file

---
+++ ApplicationDataHandler/JsonApplicationDataHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationDataHandler
+{
+    public class JsonApplicationDataHandler : IApplicationDataHandler
+    {
+        public int GetActiveUserId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetStoreCredentialsFlag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetUserIds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserMetaData SetActiveUser(string login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetStoreCredentialsFlag(bool storeCredentialsFlag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUsersList(List<UserMetaData> users)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
